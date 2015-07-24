499aa8ef6f03806eb058a653af4fccdce2567546
Sun Longfei <18910264200@qq.com>
2015-07-24 16:06:32 +0800

    测试注释

--- app/src/main/java/com/longfeisun/app/helloandroid/MainActivity.java
+++ app/src/main/java/com/longfeisun/app/helloandroid/MainActivity.java
@@ -2,8 +2,6 @@ package com.longfeisun.app.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
